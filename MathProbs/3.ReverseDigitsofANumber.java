fbf14ed001c0ec56ba1711cc4323bd8f6a5bac4c
Aiswarya R <121755513+Aiswaryaaravi@users.noreply.github.com>
2025-08-30 20:33:01 +0530

    Update 3.ReverseDigitsofANumber.java

--- MathProbs/3.ReverseDigitsofANumber.java
+++ MathProbs/3.ReverseDigitsofANumber.java
@@ -5,16 +5,25 @@ Note: If a number has trailing zeros, then its reverse will not include them.
 For e.g., reverse of 10400 will be 401 instead of 00401.
 *******************************************************************************/
 
-public class Main
-{
-	public static void main(String[] args) {
-		int n=10400;
-		int rev=0;
-		while(n!=0){
-		    int dig=n%10;
-		    rev=(rev*10)+dig;
-		    n/=10;
-		}
-		System.out.println(rev);
-	}
+class Solution {
+    public int reverse(int x) {
+        int rev=0;
+        boolean neg=x<0;
+        x=Math.abs(x);
+        while(x!=0){
+            int dig=x%10;
+            if (rev > (Integer.MAX_VALUE - dig) / 10) {
+            return 0; 
+        }
+            rev=(rev*10)+dig;
+            x/=10;
+        }
+        if(neg){
+            return -rev;
+        }
+        else{
+            return rev;
+        }
+        
+    }
 }
